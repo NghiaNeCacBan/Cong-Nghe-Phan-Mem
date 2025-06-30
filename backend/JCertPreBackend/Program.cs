8c7f893a04e38052e3696d5a9276c3da07264b14
th4ighn <101034238+th4ighn@users.noreply.github.com>
2025-06-30 09:30:40 +0700

    Thêm frontend đăng nhập cơ bản

--- backend/JCertPreBackend/Program.cs
+++ backend/JCertPreBackend/Program.cs
@@ -1,26 +1,26 @@
 // backend/JCertPreBackend/Program.cs
-  using Microsoft.EntityFrameworkCore;
-  using JCertPreBackend.Data;
+     using Microsoft.EntityFrameworkCore;
+     using JCertPreBackend.Data;
 
-  var builder = WebApplication.CreateBuilder(args);
-  builder.Services.AddControllers();
-  builder.Services.AddDbContext<JCertPreContext>(options => options.UseInMemoryDatabase("JCertPreDb"));
-  builder.Services.AddCors(options =>
-  {
-      options.AddPolicy("AllowAll", builder =>
-          builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-  });
-  builder.Services.AddEndpointsApiExplorer();
-  builder.Services.AddSwaggerGen();
+     var builder = WebApplication.CreateBuilder(args);
+     builder.Services.AddControllers();
+     builder.Services.AddDbContext<JCertPreContext>(options => options.UseInMemoryDatabase("JCertPreDb"));
+     builder.Services.AddCors(options =>
+     {
+         options.AddPolicy("AllowAll", builder =>
+             builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+     });
+     builder.Services.AddEndpointsApiExplorer();
+     builder.Services.AddSwaggerGen();
 
-  var app = builder.Build();
-  if (app.Environment.IsDevelopment())
-  {
-      app.UseSwagger();
-      app.UseSwaggerUI();
-  }
-  app.UseCors("AllowAll");
-  app.UseHttpsRedirection();
-  app.UseAuthorization();
-  app.MapControllers();
-  app.Run();
+     var app = builder.Build();
+     if (app.Environment.IsDevelopment())
+     {
+         app.UseSwagger();
+         app.UseSwaggerUI();
+     }
+     app.UseCors("AllowAll");
+     app.UseHttpsRedirection();
+     app.UseAuthorization();
+     app.MapControllers();
+     app.Run();
